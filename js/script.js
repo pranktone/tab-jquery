984dd9619566711cfec29fb0ecb4f5b3843f3e47
pranktone <pranktone@gmail.com>
2020-03-04 12:24:05 +0900

    Initial Commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
-document.addEventListener('DOMContentLoaded', () => {
+$(function(){
+    $('.js-tab-trigger').on('click', function (e) {
+        // すべてのタブメニューの'is-active'クラスを削除
+        $('.js-tab-trigger').removeClass('is-active');
+        // クリックしたタブメニューに'is-active'クラスを追加
+        $(this).addClass('is-active');
 
+        // ターゲットとなる要素（タブメニューdata属性値と等しいid値を持つコンテンツ要素[ターゲット要素]）を取得
+        let currentContent = $('#' + $(this).data('id'));
+
+        // タブコンテンツを非アクティブにする
+        $('.js-tab-target').removeClass('is-active');
+
+        // 対象コンテンツ(指定したIDの要素があったら)を表示させる
+        if (currentContent.length) {
+            currentContent.addClass('is-active');
+        }
+    });
 });
